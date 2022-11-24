28acdfca2c4a520078926f0ed66513b823860d62
Sanjay Mali <malisanjay5433@gmail.com>
2022-11-24 19:39:59 +0530

    first ember app demo with capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.helloworld.com',
+  appName: 'hello-world',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
